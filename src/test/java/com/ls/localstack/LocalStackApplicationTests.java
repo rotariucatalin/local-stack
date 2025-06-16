1a890d5d04b87e09ba3c63dd1003e41a44bafc9c
rotariucatalin <rotariucatalin90@gmail.com>
2025-06-16 17:07:26 +0300

    Initial commit

---
+++ src/test/java/com/ls/localstack/LocalStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ls.localstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
